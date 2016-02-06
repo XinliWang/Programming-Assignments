f630999add514ef503bc173de87c8b280f47a4eb
Xinli Wang <wxlmaple@hotmail.com>
2016-02-06 17:14:01 -0500

    second assignment

---
+++ Programming Assignment 2/Controllers/WeatherController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class WeatherController extends Controller
+{
+    public function index(){
+        return "Hello from index page22";
+    }
+
+    public function getWeather($location){
+        return "Hello from" . $location;
+    }
+}
